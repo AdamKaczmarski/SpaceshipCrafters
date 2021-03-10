d554425b2313c7b2f1aa7b9ee63560ac7e23c9cb
AdamKaczmarski <71774507+AdamKaczmarski@users.noreply.github.com>
2021-03-10 21:35:16 +0100

    updated one a tag

--- containers/Navbar/navbar.php
+++ containers/Navbar/navbar.php
@@ -1,7 +1,7 @@
 <header class="mb-auto">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a  href="#" class="navbar-brand">
+        <a  href="./index.php" class="navbar-brand">
             <img src="./assets/images/clipart-rocket-logo-1-transparent-white.png" alt=""width="30" height="30"
                 class="d-inline-block align-top"/>
             Spaceship Crafters
@@ -13,7 +13,7 @@
         <div class="collapse navbar-collapse justify-content-end" id="navbarMenu">
             <ul class="navbar-nav ml-auto">
                 <li class="navbar-item">
-                    <a href="#" class="nav-link active" aria-current="page">Home</a>
+                    <a href="./index.php" class="nav-link active" aria-current="page">Home</a>
                 </li>
                 <li class="navbar-item">
                     <a href="#" class="nav-link">About</a>
