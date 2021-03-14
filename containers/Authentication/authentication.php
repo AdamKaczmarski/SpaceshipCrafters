9e1736a21276e191c1b8deebe48c413f0e16c5d4
AdamKaczmarski <71774507+AdamKaczmarski@users.noreply.github.com>
2021-03-14 12:43:56 +0100

    authentication

--- containers/Authentication/authentication.php
+++ containers/Authentication/authentication.php
@@ -2,22 +2,15 @@
 if (isset($_POST['btnLogin']) && isset($_POST['username']) && isset($_POST['password']) && $_POST['username']!="" && $_POST['password']!=""){
     require("./../../db_operations/db_conn.php");
     $username = $conn->real_escape_string(trim(@$_POST['username']));
-    $hpass = md5($conn->real_escape_string(trim($_POST['password'])));
-    echo "<script>console.log('$hpass')</script>";
-    
-    $query = "SELECT username FROM adbt214_users WHERE username = '$username' AND password='$hpass'";
-    echo "<script>console.log('kurwa')</script>";
-    if ($conn->query($query)){
-        echo "<script>console.log('kurwa')</script>";
-    }
-    //$result = $conn->query($query);
-    //$rows = $result->num_rows;
-    /*if (mysqli_num_rows($result)){
-        $user = $result->fetch_assoc();
+    $hpass = md5($conn->real_escape_string(trim(@$_POST['password'])));
+    $query = "SELECT username FROM adbt214_users WHERE username = '$username' AND password='$hpass';";
+    $result = $conn->query($query);
+    if ($result->num_rows==1){
+        $user=$result->fetch_assoc();
         header('Location: ../../contact.php?'.$user['username']);
     } else {
-        header('Location: ../../index.php?logFail&'.$user['username']);
-    }*/
+        header('Location: ../../index.php?logFail');
+    }
     require("./../../db_operations/db_close.php");
 } else  {
     header('Location: ../../index.php?logEmpty');
