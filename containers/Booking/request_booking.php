950fcd5246ba7351b0c0bdc3fd9b131e30376553
AdamKaczmarski <71774507+AdamKaczmarski@users.noreply.github.com>
2021-03-18 00:39:53 +0100

    inputs

--- containers/Booking/request_booking.php
+++ containers/Booking/request_booking.php
@@ -1,5 +1,10 @@
 <form role="form" method="post">
     <div class="mb-3">
-        <input type="text">
+        <label for="bookingTitle">Please shortly describe your needs for the appointment</label>
+        <input type="text" class="form-control" name="bookingTitle" id="bookingTitle">
+    </div>
+    <div class="mb-3">
+        <label for="bookingDate">Please select a date</label>
+        <input type="date" class="form-control" name="bookingDate" id="bookingDate">
     </div>
 </form>
