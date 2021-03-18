0f41aba345355797416d2ff6b80a0637d792c6f0
AdamKaczmarski <71774507+AdamKaczmarski@users.noreply.github.com>
2021-03-18 19:20:37 +0100

    made booking

--- containers/Booking/request_booking.php
+++ containers/Booking/request_booking.php
@@ -1,10 +1,27 @@
-<form role="form" method="post">
-    <div class="mb-3">
-        <label for="bookingTitle">Please shortly describe your needs for the appointment</label>
-        <input type="text" class="form-control" name="bookingTitle" id="bookingTitle">
+
+<form role="form" method="post" class="align-items-center w-50 mx-auto"">
+    <div class="col pt-3">
+            <label for="bookingTitle">Please shortly describe your needs for the appointment</label>
+            <input type="text" class="form-control" name="bookingTitle" id="bookingTitle">
     </div>
-    <div class="mb-3">
-        <label for="bookingDate">Please select a date</label>
-        <input type="date" class="form-control" name="bookingDate" id="bookingDate">
+    <div class="row">
+        <div class="col-lg-6  col-md-12 pt-3">
+                <label for="bookingDate">Please select the date</label>
+                <input type="date" class="form-control" name="bookingDate" id="bookingDate" onchange="bookingDateHandler()">
+        </div>
+        <div class="col-lg-6  col-md-12 pt-3">
+                <label for="bookingTime">Please select the time</label>
+                <select name="bookingTime" id="bookingTime" class="form-select" disabled onchange="bookingTimeHandler()">
+                    <option selected>Choose the time</option>
+                </select>
+        </div>
+        <div class="row">
+            <div class="col  pt-3">
+                <input type="text" style="display:none" name="path" value="<?php echo $_SERVER['PHP_SELF'] ?>">
+                <button type="button" name="btnApp" id="btnApp" class="btn btn-primary" onclick="insertBooking()" disabled>Request an appointment</button>
+            </div>
+        </div>
     </div>
 </form>
+
+
--- js/bookingContent.js
+++ js/bookingContent.js
@@ -1,9 +1,50 @@
 $(document).ready(()=>{
  $('#bookApp').click(()=>{
-     $('#bookingContent').html("dup[a");
+     $('#bookingContent').load('./containers/Booking/request_booking.php');
  })
  $('#bookHis').click(()=>{
      $('#bookingContent').load('./containers/Booking/booking_history.php');
      //$('#bookingContent').html('elo');
  })
-})
+ });
+ 
+ const bookingDateHandler =()=>{
+     const date = $("#bookingDate").val();
+     $.ajax({
+         url: "./containers/Booking/open_hours.php",
+         type: "post",
+         data: {
+             date:date
+         },
+         dataType:'html',
+         success: (response)=>{
+             $('#bookingTime').append(response);
+         }
+     })
+     $('#bookingTime').removeAttr("disabled");
+ }
+
+ const bookingTimeHandler=()=>{
+    $('#btnApp').removeAttr("disabled");
+ }
+
+ const insertBooking=()=>{
+    if ($('#bookingTitle').val().length>5 && $('#bookingTime').val().length>1 && $('#bookingDate').val().length>1){
+        let title = $('#bookingTitle').val();
+        let date = $('#bookingDate').val();
+        let time = $('#bookingTime').val();
+        $.ajax({
+            url:"./containers/Booking/insert_booking.php",
+            type:'post',
+            data:{
+                title:title,
+                date:date,
+                time:time
+            },
+            datatype:'json',
+            success: function(){
+                $('#bookingContent').load('./containers/Booking/booking_history.php');
+            }
+        });
+    }
+ }
