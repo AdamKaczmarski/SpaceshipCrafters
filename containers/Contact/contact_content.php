e662b248a9026a971482956edc05f0157a810739
AKaczmarski <adam.kaczmarski@city.ac.uk>
2021-03-19 16:17:05 +0000

    Update contact_content.php

--- containers/Contact/contact_content.php
+++ containers/Contact/contact_content.php
@@ -4,15 +4,29 @@
         <h2 class="display-4">Contact us</h2>
     </div>
 </div>
-<div class="row justify-content-center">
+<div class="row justify-content-center text-justify">
     <div class="col-lg-4">
-        <p class="lead">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, corporis, praesentium voluptate itaque, repudiandae architecto non laboriosam quo minima delectus quis doloremque ipsa doloribus aliquid temporibus? Numquam aperiam aspernatur voluptate!
+        <p class="lead" style="text-align:left;">
+            Email: contact@spaceshipcrafters.com<br>
+            Phone Number: 00 44 7470 123345<br>
+            Address: 1 Rocket Rd, Hawthorne, CA 90250, USA<br>
+            <br>
+            Opening Hours:<br>
+            Monday: 9:00-16:00<br>
+            Tuesday: 9:00-16:00<br>
+            Wednesday: 9:00-16:00<br>
+            Thursday: 9:00-16:00<br>
+            Friday: 9:00-16:00
         </p>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
+        <?php if (!isset($_SESSION['loggedUser'])){
+           ?> 
+           <p class="text-lead">You can also use our form to contact us, you're only needed to register!</p>
+           <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
+         <?php } else { ?>
+            <a href="./booking.php" ><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#registerModal">Booking</button></a>
+         <?php } ?>
     </div>
     <div class="col-lg-6">
-        <!--<p>TBD RESPONSIVENESS: https://bootstrapstudio.io/tutorials/google-maps</p>-->
         <div class="ratio ratio-4x3 my-3">
          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13243.179055458959!2d-118.3280263!3d33.9206814!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5589bf4232c10232!2sSpacex!5e0!3m2!1spl!2spl!4v1615569799831!5m2!1spl!2spl"  style="border:0;"  loading="lazy"></iframe>
 
